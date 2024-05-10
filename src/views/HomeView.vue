fce7370e42ef77349ffe442197ce778bf42fbef5
kazuaki-ito <kazuaki-ito@isoshi-moustache.com>
2024-05-10 16:35:01 +0900

    format

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
 
 interface Item {
   id: number
@@ -8,23 +8,23 @@ interface Item {
 }
 
 const items = ref<Item[]>([
-  {id: 1, name: 'Item 1', selected: false},
-  {id: 2, name: 'Item 2', selected: true},
-  {id: 3, name: 'Item 3', selected: false},
+  { id: 1, name: 'Item 1', selected: false },
+  { id: 2, name: 'Item 2', selected: true },
+  { id: 3, name: 'Item 3', selected: false }
 ])
 
 const toggleSelection = (index: number) => {
-  items.value[index].checked = !items.value[index].checked;
+  items.value[index].checked = !items.value[index].checked
 }
 
 const updateItems = () => {
   items.value = [
-    {id: 4, name: 'Item 4', selected: false},
-    {id: 5, name: 'Item 5', selected: true},
+    { id: 4, name: 'Item 4', selected: false },
+    { id: 5, name: 'Item 5', selected: true }
   ]
 }
 
-const initScreen = () =>{
+const initScreen = () => {
   window.location.reload()
 }
 </script>
@@ -35,27 +35,34 @@ const initScreen = () =>{
     <a href="#" @click="initScreen">初期化</a>
     <table class="table">
       <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Selected</th>
-      </tr>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Selected</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="(item, index) in items" :key="item.id">
-        <td>{{ item.id }}</td>
-        <td>{{ item.name }}</td>
-        <td>
-          <input type="checkbox" id="item.id" v-model="item.selected" @change="toggleSelection(index)">
-        </td>
-      </tr>
+        <tr v-for="(item, index) in items" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>
+            <input
+              type="checkbox"
+              id="item.id"
+              v-model="item.selected"
+              @change="toggleSelection(index)"
+            />
+          </td>
+        </tr>
       </tbody>
     </table>
   </main>
 </template>
 
 <style>
-.table, td, th {
+.table,
+td,
+th {
   border: 2px black solid;
 }
 </style>
